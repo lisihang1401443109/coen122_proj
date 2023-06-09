eac85843c61ef081b51fe5ed164417ade26f09a2
Vobbukyo <lisihang1401443109@gmail.com>
2023-06-09 16:06:26 -0700

    ...

--- units/control.v
+++ units/control.v
@@ -1,10 +1,211 @@
 module control (
-    opcode, RegWrt, imme, PCtoReg, MemtoReg, BranchNeg, BranchZero, Jump, JumpMem, ALUop, MemRead, MemWrt
+    clk, opcode, regWrite, imme, PCtoReg, MemtoReg, branchN, branchZ, Jump, jumpMem, ALUop, memRead, memWrite
 );
 
 input [3:0] opcode;
 output RegWrt, imme, PCtoReg, MemtoReg, BranchNeg, BranchZero, Jump, JumpMem, ALUop, MemRead, MemWrt
+output [1:0] ALUop
 
 // bunch of if's
+  always @(posedge clk)
+  begin
+    case (opcode)
+      4'b0000: // opcode is 0000
+        begin
+          regWrite  = 0;
+          memToReg  = 0;
+          PCtoReg   = 0;
+          branchN   = 0;
+          branchZ   = 0;
+          Jump      = 0;
+          jumpMem   = 0;
+          memRead   = 0;
+          memWrite  = 0;
+          imme      = 0;
+          ALUop     = 2'b00;
+        end
+
+      4'b1111: // opcode is 1111
+        begin
+          regWrite  = 0;
+          memToReg  = 0;
+          PCtoReg   = 0;
+          branchN   = 0;
+          branchZ   = 0;
+          Jump      = 0;
+          jumpMem   = 0;
+          memRead   = 0;
+          memWrite  = 0;
+          imme      = 0;
+          ALUop     = 2'b01;
+        end
+
+      4'b1110: // opcode is 1110
+        begin
+          regWrite  = 1;
+          memToReg  = 1;
+          PCtoReg   = 0;
+          branchN   = 0;
+          branchZ   = 0;
+          Jump      = 0;
+          jumpMem   = 0;
+          memRead   = 0;
+          memWrite  = 1;
+          imme      = 0;
+          ALUop     = 2'b00;
+        end
+
+      4'b0011: // opcode is 0011
+        begin
+          regWrite  = 0;
+          memToReg  = 0;
+          PCtoReg   = 0;
+          branchN   = 0;
+          branchZ   = 0;
+          Jump      = 0;
+          jumpMem   = 0;
+          memRead   = 0;
+          memWrite  = 1;
+          imme      = 0;
+          ALUop     = 2'b00;
+        end
+
+      4'b0100: // opcode is 0100
+        begin
+          regWrite  = 1;
+          memToReg  = 0;
+          PCtoReg   = 0;
+          branchN   = 0;
+          branchZ   = 0;
+          Jump      = 0;
+          jumpMem   = 0;
+          memRead   = 0;
+          memWrite  = 0;
+          imme      = 0;
+          ALUop     = 2'b01;
+        end
+
+      4'b0101: // opcode is 0101
+        begin
+          regWrite  = 1;
+          memToReg  = 0;
+          PCtoReg   = 0;
+          branchN   = 0;
+          branchZ   = 0;
+          Jump      = 0;
+          jumpMem   = 0;
+          memRead   = 0;
+          memWrite  = 0;
+          imme      = 0;
+          ALUop     = 2'b01;
+        end
+
+      4'b0110: // opcode is 0110
+  begin
+          regWrite  = 1;
+          memToReg  = 0;
+          PCtoReg   = 0;
+          branchN   = 0;
+          branchZ   = 0;
+          Jump      = 0;
+          jumpMem   = 0;
+          memRead   = 0;
+          memWrite  = 0;
+          imme      = 0;
+          ALUop     = 2'b10;
+        end
+
+      4'b0111: // opcode is 0111
+        begin
+          regWrite  = 1;
+          memToReg  = 0;
+          PCtoReg   = 0;
+          branchN   = 0;
+          branchZ   = 0;
+          Jump      = 0;
+          jumpMem   = 0;
+          memRead   = 0;
+          memWrite  = 0;
+          imme      = 0;
+          ALUop     = 2'b11;
+        end
+
+      4'b1000: // opcode is 1000
+        begin
+          regWrite  = 0;
+          memToReg  = 0;
+          PCtoReg   = 0;
+          branchN   = 0;
+          branchZ   = 0;
+          Jump      = 0;
+          jumpMem   = 1;
+          memRead   = 1;
+          memWrite  = 0;
+          imme      = 1;
+          ALUop     = 2'b00;
+        end
+
+      4'b1001: // opcode is 1001
+        begin
+          regWrite  = 0;
+          memToReg  = 0;
+          PCtoReg   = 0;
+          branchN   = 0;
+          branchZ   = 1 ;
+          Jump      = 1;
+          jumpMem   = 1;
+          memRead   = 1;
+          memWrite  = 0;
+          imme      = 0;
+          ALUop     = 2'b00;
+        end
+
+      4'b1010: // opcode is 1010
+        begin
+          regWrite  = 0;
+          memToReg  = 0;
+          PCtoReg   = 0;
+          branchN   = 0;
+          branchZ   = 0;
+          Jump      = 0;
+          jumpMem   = 0;
+          memRead   = 1;
+          memWrite  = 1;
+          imme      = 1;
+          ALUop     = 2'b10;
+        end
+
+      4'b1011: // opcode is 1011
+        begin
+          regWrite  = 0;
+          memToReg  = 0;
+          PCtoReg   = 0;
+          branchN   = 1;
+          branchZ   = 0;
+          Jump      = 0;
+          jumpMem   = 0;
+          memRead   = 1;
+          memWrite  = 0;
+          imme      = 0;
+          ALUop     = 2'b00;
+        end
+
+      default: // for any other opcode
+        begin
+          // Assign default values here
+          regWrite  = 0;
+          memToReg  = 0;
+          PCtoReg   = 0;
+          branchN   = 0;
+          branchZ   = 0;
+          Jump      = 0;
+          jumpMem   = 0;
+          memRead   = 0;
+          memWrite  = 0;
+          imme      = 0;
+          ALUop     = 2'b00;
+        end
+    endcase
+  end
     
 endmodule
--- units/datapath.v
+++ units/datapath.v
@@ -7,12 +7,38 @@ wire [7:0] pc_in, pc_out;
 
 wire [31:0] inst_out;
 
-PC(pc_in, pc_out);
+wire id_regwrt, id_imme, id_PCtoReg, id_MemtoReg, id_BranchN, id_BranchZ, id_Jump, id_JumpMem, id_ALUop, id_MemRead, id_MemWrt
 
-inst_mem(clk, pc_out, inst_out);
+PC my_pc(pc_in, pc_out);
 
-IFID(clk, inst_out, inst_ifid_out, pc_out, pc_ifid_out);
+inst_mem my_inst_mem(clk, pc_out, inst_out);
 
-control(inst_ifid_out, id_regwrt, id_imme, id_PCtoReg, id_MemtoReg, id_BranchN, id_BranchZ, id_Jump, id_JumpMem, id_ALUop, id_MemRead, id_MemWrt);
+IFID my_ifid(clk, inst_out, inst_ifid_out, pc_out, pc_ifid_out);
+
+wire [3:0] opcode;
+assign opcode = inst_ifid_out[31:28];
+
+control my_control(clk, inst_ifid_out, id_regwrt, id_imme, id_PCtoReg, id_MemtoReg, id_BranchN, id_BranchZ, id_Jump, id_JumpMem, id_ALUop, id_MemRead, id_MemWrt);
+
+
+wire [5:0] rs, rt, rd;
+assign rs = inst_ifid_out[21:16];
+assign rt = inst_ifid_out[15:10];
+
+wire [31:0] id_xrs, id_xrt, Y;
+
+register_file(clk, rs, rt, rd, data_in, id_regwrt, id_xrs, id_xrt);
+
+sign_extender(clk, inst_ifid_out[21:0], Y);
+
+adder_32bit adder_id(.A(pc_ifid_out), .B(Y), .S(pc_y));
+
+IDEX my_idex()
+
+
+
+
+
+
     
 endmodule
